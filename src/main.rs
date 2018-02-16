4ecbe577aaf1fed838302a0a3eee408b5ac67332
Lucas Kent <rubickent@gmail.com>
2018-02-16 18:52:57 +1100

    Set fighter attributes

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ extern crate byteorder;
 use std::fs;
 
 use pf_sandbox::package::Package;
-use pf_sandbox::fighter::Fighter;
+use pf_sandbox::fighter::*;
 
 pub mod parse;
+
+use parse::SectionData;
 
 fn main() {
     match fs::read_dir("data/brawl/fighter") {
@@ -20,6 +22,83 @@ fn main() {
             for brawl_fighter in brawl_fighters {
                 let mut fighter = Fighter::default();
                 fighter.name = brawl_fighter.cased_fighter_name.clone();
+
+                for sub_arc in brawl_fighter.arc.sub_arc {
+                    if let Some(data) = sub_arc.data {
+                        for section in data.sections {
+                            if let SectionData::FighterData { attributes, .. } = section.data {
+                                fighter.air_jumps = attributes.num_jumps as u64 - 1;
+                                fighter.weight = attributes.weight;
+                                fighter.gravity = -attributes.gravity;
+                                fighter.terminal_vel = -attributes.term_vel;
+                                fighter.fastfall_terminal_vel = -attributes.fastfall_velocity;
+                                fighter.jump_y_init_vel = attributes.jump_y_init_vel;
+                                fighter.jump_y_init_vel_short = attributes.jump_y_init_vel_short;
+                                fighter.jump_x_init_vel = attributes.jump_x_init_vel;
+                                fighter.jump_x_term_vel = attributes.jump_x_init_term_vel;
+                                fighter.jump_x_vel_ground_mult = attributes.jump_x_vel_ground_mult;
+                                fighter.air_mobility_a = attributes.air_mobility_a;
+                                fighter.air_mobility_b = attributes.air_mobility_b;
+                                fighter.air_x_term_vel = attributes.air_x_term_vel;
+                                fighter.air_friction = attributes.air_friction_x;
+                                fighter.air_jump_x_vel = 1.0; // attributes.air_jump_x_mult; // TODO: store air_jump in an enum to allow both brawl and melee physics
+                                fighter.air_jump_y_vel = 2.0; // attributes.air_jump_y_mult;
+                                fighter.walk_init_vel = attributes.walk_init_vel;
+                                fighter.walk_acc = attributes.walk_acc;
+                                fighter.walk_max_vel = attributes.walk_max_vel;
+                                fighter.slow_walk_max_vel = 0.0_f32.max(attributes.walk_max_vel - 0.5);
+                                fighter.dash_init_vel = attributes.dash_init_vel;
+                                fighter.dash_run_acc_a = attributes.dash_run_acc_a;
+                                fighter.dash_run_acc_b = attributes.dash_run_acc_b;
+                                fighter.dash_run_term_vel = attributes.dash_run_term_vel;
+                                fighter.friction = attributes.ground_friction;
+                                // fighter.aerialdodge_mult = // TODO: PM custom?
+                                // fighter.aerialdodge_drift_frame = attributes. // TODO: PM custom?
+                                fighter.forward_roll = true;
+                                fighter.backward_roll = true;
+                                fighter.spot_dodge = true;
+                                fighter.lcancel = None; // TODO: handle for PM
+                                fighter.shield = Some(Shield {
+                                    // TODO: shield_strength !??!?!
+                                    stick_lock: false,
+                                    stick_mult: 3.0,
+                                    offset_x:   1.0,
+                                    offset_y:   8.0,
+                                    break_vel:  attributes.shield_break_vel,
+                                    scaling:    attributes.shield_size,
+                                    hp_scaling: 1.15,
+                                    hp_max:     60.0,
+                                    hp_regen:   0.1,
+                                    hp_cost:    0.28,
+                                });
+                                fighter.power_shield = Some(PowerShield {
+                                    reflect_window: Some(2),
+                                    parry: Some(PowerShieldEffect {
+                                        window: 4,
+                                        duration: 4,
+                                    }),
+                                    enemy_stun: None,
+                                });
+                                fighter.tech = Some(Tech::default());
+                                fighter.missed_tech_forced_getup = Some(220); // TODO
+                                fighter.run_turn_flip_dir_frame = attributes.flip_dir_frame as u64; // TODO
+                                fighter.tilt_turn_flip_dir_frame = attributes.flip_dir_frame as u64;
+                                fighter.tilt_turn_into_dash_iasa = attributes.flip_dir_frame as u64;
+                            }
+                        }
+                    }
+                }
+
+                for action in fighter.actions.iter_mut() {
+                    for frame in action.frames.iter_mut() {
+                        frame.colboxes.insert(0, CollisionBox {
+                            point: (0.0, 4.0),
+                            radius: 4.0,
+                            role: CollisionBoxRole::Hurt (HurtBox::default()),
+                        });
+                    }
+                }
+
                 package.fighters.push(brawl_fighter.cased_fighter_name, fighter);
             }
 
--- src/parse.rs
+++ src/parse.rs
@@ -103,25 +103,25 @@ fn arc_sakurai(data: &[u8]) -> ArcSakurai {
 #[derive(Debug)]
 pub struct BrawlFighter {
     pub cased_fighter_name: String,
-    folder_name: String,
-    arc: Arc,
+        folder_name: String,
+    pub arc: Arc,
 }
 
 const ARC_MAIN_HEADER_SIZE: usize = 0x40;
 #[derive(Debug)]
-struct Arc {
-    sub_arc: Vec<SubArc>,
+pub struct Arc {
+    pub sub_arc: Vec<SubArc>,
 }
 
 const SUB_ARC_HEADER_SIZE: usize = 0x20;
 #[derive(Debug)]
-struct SubArc {
+pub struct SubArc {
     ty: i16,
     index: i16,
     size: i32,
     group_index: u8,
     redirect_index: i16, // The index of a different file to read
-    data: Option<ArcSakurai>, // TODO: make this a custom enum
+    pub data: Option<ArcSakurai>, // TODO: make this a custom enum
 }
 
 impl SubArc {
@@ -139,13 +139,13 @@ impl SubArc {
 
 const ARC_SAKURAI_HEADER_SIZE: usize = 0x20;
 #[derive(Debug)]
-struct ArcSakurai {
+pub struct ArcSakurai {
     size: i32,
     lookup_offset: i32,
     lookup_entry_count: i32,
     section_count: i32,
     external_subroutine_count: i32,
-    sections: Vec<ArcSakuraiSection>,
+    pub sections: Vec<ArcSakuraiSection>,
 }
 
 impl ArcSakurai {
@@ -163,11 +163,11 @@ impl ArcSakurai {
 
 const ARC_SAKURAI_SECTION_HEADER_SIZE: usize = 0x8;
 #[derive(Debug)]
-struct ArcSakuraiSection {
+pub struct ArcSakuraiSection {
     data_offset: i32,
     string_offset: i32,
     name: String,
-    data: SectionData,
+    pub data: SectionData,
 }
 
 impl ArcSakuraiSection {
@@ -182,14 +182,14 @@ impl ArcSakuraiSection {
 }
 
 #[derive(Debug)]
-enum SectionData {
+pub enum SectionData {
     FighterData { header: ArcFighterData, attributes: FighterAttributes },
     None,
 }
 
 const _ARC_FIGHTER_DATA_HEADER_SIZE: usize = 0x7c;
 #[derive(Debug)]
-struct ArcFighterData {
+pub struct ArcFighterData {
     subaction_flags_start: i32,
     model_visibility_start: i32,
     attribute_start: i32,
@@ -252,56 +252,158 @@ impl ArcFighterData {
 }
 
 #[derive(Debug)]
-struct FighterAttributes {
-    walk_init_vel: f32,
-    walk_acc: f32,
-    walk_max_vel: f32,
-    ground_friction: f32,
-    dash_init_vel: f32,
-    dash_run_accel_a: f32,
-    dash_run_accel_b: f32,
-    dash_run_term_vel: f32,
-    grounded_max_x_vel: f32,
-    dash_cancel_frame_window: i32, // spreadsheet is unsure
-    guard_on_max_momentum: f32,
-    jump_squat_frames: i32,
-    jump_x_init_vel: f32,
-    jump_y_init_vel: f32,
-    jump_x_vel_ground_mult: f32,
-    jump_x_init_term_vel: f32, // TODO: does melee include this max in name?
-    jump_y_init_vel_short: f32,
-    air_jump_x_mult: f32,
-    air_jump_y_mult: f32,
-    footstool_init_vel: f32,
-    footstool_init_vel_short: f32,
-    meteor_cancel_delay: f32,
+pub struct FighterAttributes {
+    pub walk_init_vel: f32,
+    pub walk_acc: f32,
+    pub walk_max_vel: f32,
+    pub ground_friction: f32,
+    pub dash_init_vel: f32,
+    pub dash_run_acc_a: f32,
+    pub dash_run_acc_b: f32,
+    pub dash_run_term_vel: f32,
+    pub grounded_max_x_vel: f32,
+    pub dash_cancel_frame_window: i32, // spreadsheet is unsure
+    pub guard_on_max_momentum: f32,
+    pub jump_squat_frames: i32,
+    pub jump_x_init_vel: f32,
+    pub jump_y_init_vel: f32,
+    pub jump_x_vel_ground_mult: f32,
+    pub jump_x_init_term_vel: f32, // TODO: does melee include this max in name?
+    pub jump_y_init_vel_short: f32,
+    pub air_jump_x_mult: f32,
+    pub air_jump_y_mult: f32,
+    pub footstool_init_vel: f32,
+    pub footstool_init_vel_short: f32,
+    pub meteor_cancel_delay: f32,
+    pub num_jumps: u32,
+    pub gravity: f32,
+    pub term_vel: f32,
+    pub air_friction_y: f32,
+    pub air_y_term_vel: f32,
+    pub air_mobility_a: f32,
+    pub air_mobility_b: f32,
+    pub air_x_term_mobility: f32,
+    pub air_friction_x: f32,
+    pub fastfall_velocity: f32,
+    pub air_x_term_vel: f32,
+    pub glide_frame_window: f32,
+    pub jab2_window: f32,
+    pub jab3_window: f32,
+    pub ftilt2_window: f32,
+    pub ftilt3_window: f32,
+    pub fsmash2_window: f32,
+    pub flip_dir_frame: f32,
+    pub weight: f32,
+    pub size: f32,
+    pub results_screen_size: f32,
+    pub shield_size: f32,
+    pub shield_break_vel: f32,
+    pub shield_strength: f32,
+    pub respawn_platform_size: f32,
+    pub edge_jump_x_vel: f32,
+    pub edge_jump_y_vel: f32,
+    pub item_throw_strength: f32,
+    pub projectile_item_move_speed: f32,
+    pub projectile_item_move_speed_dash_f: f32,
+    pub projectile_item_move_speed_dash_b: f32,
+    pub light_landing_lag: f32,
+    pub normal_landing_lag: f32,
+    pub nair_landing_lag: f32,
+    pub fair_landing_lag: f32,
+    pub bair_landing_lag: f32,
+    pub uair_landing_lag: f32,
+    pub dair_landing_lag: f32,
+    pub term_vel_hard_frames: u32,
+    pub hip_n_bone: u32, // spreadsheet is unsure
+    pub tag_height_value: f32,
+    pub walljump_x_vel: f32, // used for normal walljumps and walljump techs
+    pub walljump_y_vel: f32, // used for normal walljumps and walljump techs
+    pub lhand_n_bone: u32,
+    pub rhand_n_bone: u32,
+    pub water_y_acc: f32,
+    pub spit_star_size: f32,
+    pub spit_star_damage: u32,
+    pub egg_size: f32,
+    pub hip_n_bone2: u32,
+    pub x_rot_n_bone: u32, // bone to be grabbed from?
 }
 
 impl FighterAttributes {
     pub fn new(data: &[u8]) -> FighterAttributes {
         FighterAttributes {
-            walk_init_vel:            (&data[0x00..0x04]).read_f32::<BigEndian>().unwrap(),
-            walk_acc:                 (&data[0x04..0x08]).read_f32::<BigEndian>().unwrap(),
-            walk_max_vel:             (&data[0x08..0x0c]).read_f32::<BigEndian>().unwrap(),
-            ground_friction:          (&data[0x0c..0x10]).read_f32::<BigEndian>().unwrap(),
-            dash_init_vel:            (&data[0x10..0x14]).read_f32::<BigEndian>().unwrap(),
-            dash_run_accel_a:         (&data[0x14..0x18]).read_f32::<BigEndian>().unwrap(),
-            dash_run_accel_b:         (&data[0x18..0x1c]).read_f32::<BigEndian>().unwrap(),
-            dash_run_term_vel:        (&data[0x1c..0x20]).read_f32::<BigEndian>().unwrap(),
-            grounded_max_x_vel:       (&data[0x24..0x28]).read_f32::<BigEndian>().unwrap(),
-            dash_cancel_frame_window: (&data[0x28..0x2c]).read_i32::<BigEndian>().unwrap(),
-            guard_on_max_momentum:    (&data[0x2c..0x30]).read_f32::<BigEndian>().unwrap(),
-            jump_squat_frames:        (&data[0x30..0x34]).read_i32::<BigEndian>().unwrap(),
-            jump_x_init_vel:          (&data[0x34..0x38]).read_f32::<BigEndian>().unwrap(),
-            jump_y_init_vel:          (&data[0x38..0x3c]).read_f32::<BigEndian>().unwrap(),
-            jump_x_vel_ground_mult:   (&data[0x3c..0x40]).read_f32::<BigEndian>().unwrap(),
-            jump_x_init_term_vel:     (&data[0x40..0x44]).read_f32::<BigEndian>().unwrap(),
-            jump_y_init_vel_short:    (&data[0x44..0x48]).read_f32::<BigEndian>().unwrap(),
-            air_jump_x_mult:          (&data[0x48..0x4c]).read_f32::<BigEndian>().unwrap(),
-            air_jump_y_mult:          (&data[0x4c..0x50]).read_f32::<BigEndian>().unwrap(),
-            footstool_init_vel:       (&data[0x50..0x54]).read_f32::<BigEndian>().unwrap(),
-            footstool_init_vel_short: (&data[0x54..0x58]).read_f32::<BigEndian>().unwrap(),
-            meteor_cancel_delay:      (&data[0x5c..0x60]).read_f32::<BigEndian>().unwrap(),
+            walk_init_vel:                     (&data[0x00..0x04]).read_f32::<BigEndian>().unwrap(),
+            walk_acc:                          (&data[0x04..0x08]).read_f32::<BigEndian>().unwrap(),
+            walk_max_vel:                      (&data[0x08..0x0c]).read_f32::<BigEndian>().unwrap(),
+            ground_friction:                   (&data[0x0c..0x10]).read_f32::<BigEndian>().unwrap(),
+            dash_init_vel:                     (&data[0x10..0x14]).read_f32::<BigEndian>().unwrap(),
+            dash_run_acc_a:                  (&data[0x14..0x18]).read_f32::<BigEndian>().unwrap(),
+            dash_run_acc_b:                  (&data[0x18..0x1c]).read_f32::<BigEndian>().unwrap(),
+            dash_run_term_vel:                 (&data[0x1c..0x20]).read_f32::<BigEndian>().unwrap(),
+            grounded_max_x_vel:                (&data[0x24..0x28]).read_f32::<BigEndian>().unwrap(),
+            dash_cancel_frame_window:          (&data[0x28..0x2c]).read_i32::<BigEndian>().unwrap(),
+            guard_on_max_momentum:             (&data[0x2c..0x30]).read_f32::<BigEndian>().unwrap(),
+            jump_squat_frames:                 (&data[0x30..0x34]).read_i32::<BigEndian>().unwrap(),
+            jump_x_init_vel:                   (&data[0x34..0x38]).read_f32::<BigEndian>().unwrap(),
+            jump_y_init_vel:                   (&data[0x38..0x3c]).read_f32::<BigEndian>().unwrap(),
+            jump_x_vel_ground_mult:            (&data[0x3c..0x40]).read_f32::<BigEndian>().unwrap(),
+            jump_x_init_term_vel:              (&data[0x40..0x44]).read_f32::<BigEndian>().unwrap(),
+            jump_y_init_vel_short:             (&data[0x44..0x48]).read_f32::<BigEndian>().unwrap(),
+            air_jump_x_mult:                   (&data[0x48..0x4c]).read_f32::<BigEndian>().unwrap(),
+            air_jump_y_mult:                   (&data[0x4c..0x50]).read_f32::<BigEndian>().unwrap(),
+            footstool_init_vel:                (&data[0x50..0x54]).read_f32::<BigEndian>().unwrap(),
+            footstool_init_vel_short:          (&data[0x54..0x58]).read_f32::<BigEndian>().unwrap(),
+            meteor_cancel_delay:               (&data[0x5c..0x60]).read_f32::<BigEndian>().unwrap(),
+            num_jumps:                         (&data[0x60..0x64]).read_u32::<BigEndian>().unwrap(),
+            gravity:                           (&data[0x64..0x68]).read_f32::<BigEndian>().unwrap(),
+            term_vel:                          (&data[0x68..0x6c]).read_f32::<BigEndian>().unwrap(),
+            air_friction_y:                    (&data[0x6c..0x70]).read_f32::<BigEndian>().unwrap(),
+            air_y_term_vel:                    (&data[0x70..0x74]).read_f32::<BigEndian>().unwrap(),
+            air_mobility_a:                    (&data[0x74..0x78]).read_f32::<BigEndian>().unwrap(),
+            air_mobility_b:                    (&data[0x78..0x7c]).read_f32::<BigEndian>().unwrap(),
+            air_x_term_mobility:               (&data[0x7c..0x80]).read_f32::<BigEndian>().unwrap(),
+            air_friction_x:                    (&data[0x80..0x84]).read_f32::<BigEndian>().unwrap(),
+            fastfall_velocity:                 (&data[0x84..0x88]).read_f32::<BigEndian>().unwrap(),
+            air_x_term_vel:                    (&data[0x88..0x8c]).read_f32::<BigEndian>().unwrap(),
+            glide_frame_window:                (&data[0x8c..0x90]).read_f32::<BigEndian>().unwrap(),
+            jab2_window:                       (&data[0x94..0x98]).read_f32::<BigEndian>().unwrap(),
+            jab3_window:                       (&data[0x98..0x9c]).read_f32::<BigEndian>().unwrap(),
+            ftilt2_window:                     (&data[0x9c..0xa0]).read_f32::<BigEndian>().unwrap(),
+            ftilt3_window:                     (&data[0xa0..0xa4]).read_f32::<BigEndian>().unwrap(),
+            fsmash2_window:                    (&data[0xa4..0xa8]).read_f32::<BigEndian>().unwrap(),
+            flip_dir_frame:                    (&data[0xa8..0xac]).read_f32::<BigEndian>().unwrap(),
+            weight:                            (&data[0xb0..0xb4]).read_f32::<BigEndian>().unwrap(),
+            size:                              (&data[0xb4..0xb8]).read_f32::<BigEndian>().unwrap(),
+            results_screen_size:               (&data[0xb8..0xbc]).read_f32::<BigEndian>().unwrap(),
+            shield_size:                       (&data[0xc4..0xc8]).read_f32::<BigEndian>().unwrap(),
+            shield_break_vel:                  (&data[0xc8..0xcc]).read_f32::<BigEndian>().unwrap(),
+            shield_strength:                   (&data[0xcc..0xd0]).read_f32::<BigEndian>().unwrap(),
+            respawn_platform_size:             (&data[0xd4..0xd8]).read_f32::<BigEndian>().unwrap(),
+            edge_jump_x_vel:                   (&data[0xf4..0xf8]).read_f32::<BigEndian>().unwrap(),
+            edge_jump_y_vel:                   (&data[0xfc..0x100]).read_f32::<BigEndian>().unwrap(),
+            item_throw_strength:               (&data[0x118..0x11c]).read_f32::<BigEndian>().unwrap(),
+            projectile_item_move_speed:        (&data[0x128..0x12c]).read_f32::<BigEndian>().unwrap(),
+            projectile_item_move_speed_dash_f: (&data[0x12c..0x130]).read_f32::<BigEndian>().unwrap(),
+            projectile_item_move_speed_dash_b: (&data[0x130..0x134]).read_f32::<BigEndian>().unwrap(),
+            light_landing_lag:                 (&data[0x138..0x13c]).read_f32::<BigEndian>().unwrap(),
+            normal_landing_lag:                (&data[0x13c..0x140]).read_f32::<BigEndian>().unwrap(),
+            nair_landing_lag:                  (&data[0x140..0x144]).read_f32::<BigEndian>().unwrap(),
+            fair_landing_lag:                  (&data[0x144..0x148]).read_f32::<BigEndian>().unwrap(),
+            bair_landing_lag:                  (&data[0x148..0x14c]).read_f32::<BigEndian>().unwrap(),
+            uair_landing_lag:                  (&data[0x14c..0x150]).read_f32::<BigEndian>().unwrap(),
+            dair_landing_lag:                  (&data[0x150..0x154]).read_f32::<BigEndian>().unwrap(),
+            term_vel_hard_frames:              (&data[0x154..0x158]).read_u32::<BigEndian>().unwrap(),
+            hip_n_bone:                        (&data[0x158..0x15c]).read_u32::<BigEndian>().unwrap(),
+            tag_height_value:                  (&data[0x15c..0x160]).read_f32::<BigEndian>().unwrap(),
+            walljump_x_vel:                    (&data[0x164..0x168]).read_f32::<BigEndian>().unwrap(),
+            walljump_y_vel:                    (&data[0x168..0x16c]).read_f32::<BigEndian>().unwrap(),
+            lhand_n_bone:                      (&data[0x180..0x184]).read_u32::<BigEndian>().unwrap(),
+            rhand_n_bone:                      (&data[0x184..0x188]).read_u32::<BigEndian>().unwrap(),
+            water_y_acc:                       (&data[0x18c..0x190]).read_f32::<BigEndian>().unwrap(),
+            spit_star_size:                    (&data[0x1a4..0x1a8]).read_f32::<BigEndian>().unwrap(),
+            spit_star_damage:                  (&data[0x1a8..0x1ac]).read_u32::<BigEndian>().unwrap(),
+            egg_size:                          (&data[0x1ac..0x1b0]).read_f32::<BigEndian>().unwrap(),
+            hip_n_bone2:                       (&data[0x1cc..0x1d0]).read_u32::<BigEndian>().unwrap(),
+            x_rot_n_bone:                      (&data[0x1e0..0x1e4]).read_u32::<BigEndian>().unwrap(),
         }
     }
 }
