c068d3e7ac0b1479cdbe29c8e3c3b84a66d5d192
Rukai <rubickent@gmail.com>
2019-01-26 11:51:40 +1100

    Update deps, rust version and rust 2018 cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-             extern crate brawllib_rs;
-             extern crate cgmath;
-             extern crate enum_traits;
-             extern crate env_logger;
-             extern crate getopts;
-             extern crate noisy_float;
-             extern crate pf_sandbox_lib;
-             extern crate ref_slice;
-             extern crate treeflection;
 #[macro_use] extern crate log;
 
 mod action_map;
